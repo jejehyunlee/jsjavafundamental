f44d600e97611f119eb6f0cfb96b48294f1bc99e
jejehyunlee <jefrisaputra989@gmail.com>
2023-01-05 11:18:38 +0700

    Edit Class

--- src/main/java/com/juaracoding/jsjavafundamental/HelloTest.java
+++ src/main/java/com/juaracoding/jsjavafundamental/HelloTest.java
@@ -1,5 +1,14 @@
 package com.juaracoding.jsjavafundamental;
 
+/*
+Created By IntelliJ IDEA 2022.1.3 (Community Edition)
+@Author ${USER} a.k.a Jefri S
+Java Developer
+Createrd On ${DATE} ${TIME}
+@Last Modified ${DATE} ${TIME}
+Version 1.0
+*/
+
 public class HelloTest {
 //psvm -> tab : auto code
     public static void main(String[] args) {
