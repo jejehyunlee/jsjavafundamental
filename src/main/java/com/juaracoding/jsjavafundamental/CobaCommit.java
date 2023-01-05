b195e72b5a4143eb0706d796896605962542d666
jejehyunlee <jefrisaputra989@gmail.com>
2023-01-05 11:23:50 +0700

    Add Class

---
+++ src/main/java/com/juaracoding/jsjavafundamental/CobaCommit.java
@@ -0,0 +1,16 @@
+package com.juaracoding.jsjavafundamental;
+
+/*
+Created By IntelliJ IDEA 2022.1.3 (Community Edition)
+@Author JEJE a.k.a Jefri S
+Java Developer
+Createrd On 1/5/2023 11:22
+@Last Modified 1/5/2023 11:22
+Version 1.0
+*/
+
+public class CobaCommit {
+    public static void main(String[] args) {
+        System.out.println("COBA COMMIT");
+    }
+}
--- src/main/java/com/juaracoding/jsjavafundamental/HelloTest.java
+++ src/main/java/com/juaracoding/jsjavafundamental/HelloTest.java
@@ -1,13 +1,5 @@
 package com.juaracoding.jsjavafundamental;
 
-/*
-Created By IntelliJ IDEA 2022.1.3 (Community Edition)
-@Author JEJE a.k.a Jefri S
-Java Developer
-Createrd On 1/5/2023 11:19
-@Last Modified 1/5/2023 11:19
-Version 1.0
- */
 
 public class HelloTest {
 //psvm -> tab : auto code
