7f1f5d5822e789134e9087ffe97bb0a68ac0d04d
jejehyunlee <jefrisaputra989@gmail.com>
2023-01-05 10:56:41 +0700

    Edit Class

--- src/main/java/com/juaracoding/jsjavafundamental/HelloTest.java
+++ src/main/java/com/juaracoding/jsjavafundamental/HelloTest.java
@@ -1,7 +1,9 @@
 package com.juaracoding.jsjavafundamental;
 
 public class HelloTest {
+//psvm -> tab : auto code
     public static void main(String[] args) {
+//sout -> tab : auto code
         System.out.println("Hello Java");
     }
 }
