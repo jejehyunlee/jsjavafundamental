221e1f61c69e5c57bb8e31f52c3000b6bc006b6d
jejehyunlee <jefrisaputra989@gmail.com>
2023-01-11 22:45:41 +0700

    CobaRandom

---
+++ src/main/java/com/juaracoding/jsjavafundamental/daytujuh/CobaRandom.java
@@ -0,0 +1,35 @@
+package com.juaracoding.jsjavafundamental.daytujuh;
+
+/*
+Created By IntelliJ IDEA 2022.1.3 (Community Edition)
+@Author JEJE a.k.a Jefri S
+Java Developer
+Createrd On 1/11/2023 21:12
+@Last Modified 1/11/2023 21:12
+Version 1.0
+*/
+
+public class CobaRandom {
+
+        public static void main(String[] args) {
+            //Generate random number
+            double rand = Math.random();
+            System.out.println("Random Number :" + rand);
+
+            int max = 10;
+            int min = 1;
+            int range = max - min + 1;
+
+            //generate random numbers within 1 to 10
+            for(int i=0;i<10;i++)
+            {
+                int randInt = (int)(Math.random() * range) + min;
+
+                //Output is different everytime this code is executed
+                System.out.print(randInt+",");
+            }
+        }
+    }
+
+
+
