72fbee63e7b4e51d23e5090712191e4aec43ed45
jejehyunlee <jefrisaputra989@gmail.com>
2023-01-11 22:43:07 +0700

    CobaHandlingException

---
+++ src/main/java/com/juaracoding/jsjavafundamental/dayenam/HandlngException.java
@@ -0,0 +1,31 @@
+package com.juaracoding.jsjavafundamental.dayenam;
+
+/*
+Created By IntelliJ IDEA 2022.1.3 (Community Edition)
+@Author JEJE a.k.a Jefri S
+Java Developer
+Createrd On 1/10/2023 22:25
+@Last Modified 1/10/2023 22:25
+Version 1.0
+*/
+
+public class HandlngException {
+    public static void main(String[] args) {
+            /*
+            try {
+                //  Block of code to try
+                }
+            catch( Exception e)
+                {
+                //  Block of code to handle errors
+                }
+            */
+
+//        int time = 20;
+//
+//        String result = (time < 18) ? "Selamat siang." : "Selamat malam.";
+//
+//        System.out.println(result);
+
+    }
+}
