ee3d7d98a2bdc85d055b2e237f0fe277bd88ff13
jejehyunlee <jefrisaputra989@gmail.com>
2023-01-11 22:46:40 +0700

    CobaMath

---
+++ src/main/java/com/juaracoding/jsjavafundamental/daytujuh/CobaMath.java
@@ -0,0 +1,18 @@
+package com.juaracoding.jsjavafundamental.daytujuh;
+
+/*
+Created By IntelliJ IDEA 2022.1.3 (Community Edition)
+@Author JEJE a.k.a Jefri S
+Java Developer
+Createrd On 1/11/2023 21:21
+@Last Modified 1/11/2023 21:21
+Version 1.0
+*/
+
+public class CobaMath {
+    public static void main(String[] args) {
+
+        System.out.print((int)Math.ceil(9.000000000000000000000000001)+" DAN ");
+        System.out.print((int)Math.floor(4.6));
+    }
+}
