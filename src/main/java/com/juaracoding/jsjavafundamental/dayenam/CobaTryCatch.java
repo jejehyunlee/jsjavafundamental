9fe9a17a8c8c2537281c08257d9d25fa89b4a5cb
jejehyunlee <jefrisaputra989@gmail.com>
2023-01-11 22:45:20 +0700

    CobaTryCatch

---
+++ src/main/java/com/juaracoding/jsjavafundamental/dayenam/CobaTryCatch.java
@@ -0,0 +1,34 @@
+package com.juaracoding.jsjavafundamental.dayenam;
+
+/*
+Created By IntelliJ IDEA 2022.1.3 (Community Edition)
+@Author JEJE a.k.a Jefri S
+Java Developer
+Createrd On 1/11/2023 18:55
+@Last Modified 1/11/2023 18:55
+Version 1.0
+*/
+import java.util.Scanner;
+
+public class CobaTryCatch {
+    public static void main(String[] args) {
+
+                //deklarasi dan inisialisasi
+                Scanner sc = new Scanner(System.in);
+                int intNumber = 0, intTotal = 0;
+                boolean isAgain = true;
+
+                try {
+                    System.out.println("Masukkan angka sepuasnya: ");
+                    do {
+                        intNumber = sc.nextInt();//input
+
+                        intTotal += intNumber;//proses penjumlahan input
+                    } while (isAgain);//perulangan utk input kembali
+                } catch (Exception e) {//exception handling utk input karakter
+                    System.out.println("Jumlah: " + intTotal);//output
+                    System.exit(0);//keluar dari sistem
+                }
+            }
+        }
+
