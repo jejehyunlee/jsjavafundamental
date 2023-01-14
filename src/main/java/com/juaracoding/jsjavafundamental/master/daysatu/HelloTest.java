1073d4b627d80e322f8cbd2e879f5ce3965d272c
jejehyunlee <jefrisaputra989@gmail.com>
2023-01-15 01:14:56 +0700

    Update

--- src/main/java/com/juaracoding/jsjavafundamental/master/daysatu/HelloTest.java
+++ src/main/java/com/juaracoding/jsjavafundamental/master/daysatu/HelloTest.java
@@ -1,12 +1,144 @@
 package com.juaracoding.jsjavafundamental.master.daysatu;
-
+import java.util.Scanner;
 
 public class HelloTest {
-//psvm -> tab : auto code
+    //psvm -> tab : auto code
     public static void main(String[] args) {
 //sout -> tab : auto code
-        System.out.println("Hello Java");
 
+        /* Soal
+     1. (Input)  : Jquery 1995@v1.0
+        (Output} : 529
 
+     2. (Input}  : Bebas Lepas@
+        (Output) : 238
+
+     3. (Input)  : Tid4k $@ma
+        (Output) : ?????
+         */
+
+        //start convert all string to ASCII
+        char chaConvert = 'I';
+        int intHasil= 0;
+        int intSeluruhSatu = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Percobaan pertama");
+        System.out.print("Masukan kata no. 1 : ");
+        String inputSatu = sc.nextLine();
+        for (int i = 0; i < inputSatu.length(); i++)
+        {
+            chaConvert = inputSatu.charAt(i);
+            intHasil = chaConvert;
+            if ((intHasil >=32 && intHasil <=47) || (intHasil>=48 && intHasil<=57) || (intHasil>=58 && intHasil<=64) || (intHasil>=65 && intHasil<=90) || (intHasil>=97 && intHasil<=122) )
+            {
+                System.out.println("Kode ASCII dari "+inputSatu.charAt(i)+" adalah "+ intHasil);
+                intSeluruhSatu += intHasil;
+            }
+        }
+        System.out.println("Hasil dari input "+inputSatu +" adalah "+intSeluruhSatu);
+        System.out.println("Jumlah belum sesuai");
+        //end convert all string to ASCII
+
+        System.out.println("");
+        System.out.println("Analisa");
+        //start convert string to ASCII
+        int intTotalSatu = 0;
+        for (int i = 0; i < inputSatu.length(); i++)
+        {
+            chaConvert = inputSatu.charAt(i);
+            intHasil = chaConvert;
+            if ((intHasil >=32 && intHasil <=47) || (intHasil>=48 && intHasil<=57) || (intHasil>=58 && intHasil<=64) || (intHasil>=65 && intHasil<=90))
+            {
+                System.out.println("Kode ASCII dari "+inputSatu.charAt(i)+" adalah "+ intHasil);
+                intTotalSatu += intHasil;
+            }
+        }
+        System.out.println("Hasil dari input "+inputSatu +" adalah "+intTotalSatu);
+        System.out.println("Jumlah sesuai");
+        //End convert string to ASCII
+
+        System.out.println("");
+        System.out.println("===========================================================");
+        System.out.println("");
+
+        //start convert all string to ASCII
+        int intSeluruhDua = 0;
+        System.out.println("Percobaan kedua");
+        System.out.print("Masukan kata no. 2 : ");
+        String inputDua = sc.nextLine();
+        for (int i = 0; i < inputDua.length(); i++)
+        {
+            chaConvert = inputDua.charAt(i);
+            intHasil = chaConvert;
+            if ((intHasil >=32 && intHasil <=47) || (intHasil>=48 && intHasil<=57) || (intHasil>=58 && intHasil<=64) || (intHasil>=65 && intHasil<=90) || (intHasil>=97 && intHasil<=122) )
+            {
+                System.out.println("Kode ASCII dari "+inputDua.charAt(i)+" adalah "+intHasil);
+                intSeluruhDua += intHasil;
+            }
+        }
+        System.out.println("Hasil dari input "+inputDua +" adalah "+intSeluruhDua);
+        System.out.println("Jumlah belum sesuai");
+        //End convert all string to ASCII
+
+        System.out.println("");
+        System.out.println("Analisa");
+        //start convert string to ASCII
+        int intTotalDua = 0;
+        for (int i = 0; i < inputDua.length(); i++)
+        {
+            chaConvert = inputDua.charAt(i);
+            intHasil = chaConvert;
+            if ((intHasil >=32 && intHasil <=47) || (intHasil>=48 && intHasil<=57) || (intHasil>=58 && intHasil<=64) || (intHasil>=65 && intHasil<=90))
+            {
+                System.out.println("Kode ASCII dari "+inputDua.charAt(i)+" adalah "+intHasil);
+                intTotalDua += intHasil;
+            }
+        }
+        System.out.println("Hasil dari input "+inputDua +" adalah "+intTotalDua);
+        System.out.println("Jumlah sesuai");
+        //End convert string to ASCII
+
+        System.out.println("");
+        System.out.println("============================================================");
+        System.out.println("");
+
+        //start convert all string to ASCII
+        int intSeluruhTiga = 0;
+        System.out.print("Masukan kata no. 3 : ");
+        String inputTiga = sc.nextLine();
+        for (int i = 0; i < inputTiga.length(); i++)
+        {
+            chaConvert = inputTiga.charAt(i);
+            intHasil = chaConvert;
+            if ((intHasil >=32 && intHasil <=47) || (intHasil>=48 && intHasil<=57) || (intHasil>=58 && intHasil<=64) || (intHasil>=65 && intHasil<=90) || (intHasil>=97 && intHasil<=122) )
+            {
+                System.out.println("Kode ASCII dari "+inputTiga.charAt(i)+" adalah "+intHasil);
+                intSeluruhTiga += intHasil;
+            }
+        }
+        System.out.println("Hasil dari input "+inputTiga+" adalah "+intSeluruhTiga);
+        System.out.println("Jumlah belum sesuai");
+        //End convert all string to ASCII
+
+        System.out.println("");
+        System.out.println("Analisa - menghilangkan karakter huruf kecil ");
+
+        //Start convert string to ASCII
+        int intTotalTiga = 0;
+        for (int i = 0; i < inputTiga.length(); i++)
+        {
+            chaConvert = inputTiga.charAt(i);
+            intHasil = chaConvert;
+            if ((intHasil >=32 && intHasil <=47) || (intHasil>=48 && intHasil<=57) || (intHasil>=58 && intHasil<=64) || (intHasil>=65 && intHasil<=90))
+            {
+                System.out.println("Kode ASCII dari "+inputTiga.charAt(i)+" adalah "+intHasil);
+                intTotalTiga += intHasil;
+            }
+        }
+        System.out.println("Hasil dari input "+inputTiga+" adalah "+intTotalTiga);
+        System.out.println("Jumlah sesuai");
+        //End convert all string to ASCII
     }
+
 }
+
