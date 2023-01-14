3cb821d8bdc64dbc234fd7e85dab05d622e798ec
jejehyunlee <jefrisaputra989@gmail.com>
2023-01-15 01:23:46 +0700

    AddClass

---
+++ src/main/java/com/juaracoding/jsjavafundamental/testharian/TestArray.java
@@ -0,0 +1,17 @@
+package com.juaracoding.jsjavafundamental.testharian;
+
+/*
+Created By IntelliJ IDEA 2022.1.3 (Community Edition)
+Build #IC-221.5921.22, built on June 21, 2022
+@Author JEJE a.k.a Jefri S
+Java Developer
+Createrd On 1/15/2023 01:21
+@Last Modified 1/15/2023 01:21
+Version 1.0
+*/
+
+public class TestArray {
+    public static void main(String[] args) {
+
+    }
+}
